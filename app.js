97adb4509a469606df5a7cd047082a11023fcb2e
mglaptop <mglaptop0001@outlook.com>
2024-07-01 12:40:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("menu-button");
+const xbtn = document.getElementById("xbtn");
+
+
+
+btn.addEventListener("click", function boxmove() {
+  menu.style.marginLeft = "0px";
+ });
+
+ xbtn.addEventListener("click", function boxmove() {
+  menu.style.marginLeft = "500px";
+ });
